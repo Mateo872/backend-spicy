59cac5d4d5c83c397588440298742801adbed888
Mateo Bellini <bellinimateo872@gmail.com>
2023-12-24 20:10:13 +0100

    Configuración para subir al servidor

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 import "dotenv/config";
 
+const PASSWORD = process.env.PASSWORD;
+
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/soagrico_db";
+  process.env.MONGODB_URI ||
+  `mongodb+srv://bellinimateo872:${PASSWORD}@spicy-basics.qam2gwa.mongodb.net/?retryWrites=true&w=majority`;
