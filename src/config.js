fa17b28edb96f6d8cfd2a788298d0ceeb461b4a4
Mateo Bellini <bellinimateo872@gmail.com>
2023-12-24 20:50:30 +0100

    Configuración para subirlo en Vercel

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ const PASSWORD = process.env.PASSWORD;
 
 export const MONGODB_URI =
   process.env.MONGODB_URI ||
-  `mongodb+srv://bellinimateo872:${PASSWORD}@cluster0.dfpjt6n.mongodb.net/?retryWrites=true&w=majority`;
+  `mongodb+srv://bellinimateo872:${PASSWORD}@spicy.9cehl5g.mongodb.net/spicy-basics`;
